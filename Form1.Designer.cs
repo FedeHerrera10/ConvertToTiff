b04ac7193e9cf71cabb1681b20a4b9827ab47f45
Federico Herrera <herrera3299@gmail.com>
2017-10-19 11:17:14 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ConverToTif
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFrontal = new System.Windows.Forms.TextBox();
+            this.txtDorso = new System.Windows.Forms.TextBox();
+            this.btnFrontal = new System.Windows.Forms.Button();
+            this.btnDorso = new System.Windows.Forms.Button();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFrontal
+            // 
+            this.txtFrontal.Location = new System.Drawing.Point(149, 31);
+            this.txtFrontal.Name = "txtFrontal";
+            this.txtFrontal.ReadOnly = true;
+            this.txtFrontal.Size = new System.Drawing.Size(262, 20);
+            this.txtFrontal.TabIndex = 3;
+            // 
+            // txtDorso
+            // 
+            this.txtDorso.Location = new System.Drawing.Point(149, 84);
+            this.txtDorso.Name = "txtDorso";
+            this.txtDorso.ReadOnly = true;
+            this.txtDorso.Size = new System.Drawing.Size(262, 20);
+            this.txtDorso.TabIndex = 4;
+            // 
+            // btnFrontal
+            // 
+            this.btnFrontal.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFrontal.Location = new System.Drawing.Point(10, 28);
+            this.btnFrontal.Name = "btnFrontal";
+            this.btnFrontal.Size = new System.Drawing.Size(112, 23);
+            this.btnFrontal.TabIndex = 5;
+            this.btnFrontal.Text = "Imagen Dorso";
+            this.btnFrontal.UseVisualStyleBackColor = true;
+            this.btnFrontal.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDorso
+            // 
+            this.btnDorso.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDorso.Location = new System.Drawing.Point(10, 81);
+            this.btnDorso.Name = "btnDorso";
+            this.btnDorso.Size = new System.Drawing.Size(116, 23);
+            this.btnDorso.TabIndex = 6;
+            this.btnDorso.Text = "Imagen Frontal";
+            this.btnDorso.UseVisualStyleBackColor = true;
+            this.btnDorso.Click += new System.EventHandler(this.btnDorso_Click);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(149, 175);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(194, 31);
+            this.btnConvertir.TabIndex = 7;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(473, 245);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.btnDorso);
+            this.Controls.Add(this.btnFrontal);
+            this.Controls.Add(this.txtDorso);
+            this.Controls.Add(this.txtFrontal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Elegir Imagen";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFrontal;
+        private System.Windows.Forms.TextBox txtDorso;
+        private System.Windows.Forms.Button btnFrontal;
+        private System.Windows.Forms.Button btnDorso;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ConverToTif
+{
+    public partial class Form1 : Form
+    {
+        private string ruta_tif = ConfigurationManager.AppSettings.Get("ruta_tif");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Image Files (JPG,PNG,GIF)|*.JPG;*.PNG;*.GIF"; ;
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string path_img_frontal = openFileDialog1.FileName;
+                    txtFrontal.Text = path_img_frontal;
+                    
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: No se pudo abrir la imagen: " + ex.Message,"Error..",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnDorso_Click(object sender, EventArgs e)
+
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Image Files (JPG,PNG,GIF)|*.JPG;*.PNG;*.GIF"; ;
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string path_img_dorso = openFileDialog1.FileName;
+                    txtDorso.Text = path_img_dorso;
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: No se pudo abrir la imagen: " + ex.Message, "Error..", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+       private void DoExistingFileSave(Image[] imagenes, string loc)
+        {
+            try
+            {
+                Image[] scannedImages = new Image[2];
+                scannedImages = imagenes;
+                SaveMultipage(scannedImages, loc, "TIFF");
+                MessageBox.Show("Conversion con exito", "Exito..", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtDorso.Text = "";
+                txtFrontal.Text = "";
+
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message,"Error..",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+         private bool SaveMultipage(Image[] bmp, string location, string type)
+        {
+            if (bmp != null)
+            {
+                try
+                {
+                    ImageCodecInfo codecInfo = get_codec_forstring(type);
+
+                    for (int i = 0; i < bmp.Length; i++)
+                    {
+                        if (bmp[i] == null)
+                            break;
+                    }
+
+                    if (bmp.Length == 1)
+                    {
+
+                        EncoderParameters iparams = new EncoderParameters(1);
+                        System.Drawing.Imaging.Encoder iparam = System.Drawing.Imaging.Encoder.Compression;
+                        EncoderParameter iparamPara = new EncoderParameter(iparam, (long)(EncoderValue.CompressionCCITT4));
+                        iparams.Param[0] = iparamPara;
+                        bmp[0].Save(location, codecInfo, iparams);
+                    }
+                    else if (bmp.Length > 1)
+                    {
+                        System.Drawing.Imaging.Encoder saveEncoder;
+                        System.Drawing.Imaging.Encoder compressionEncoder;
+                        EncoderParameter SaveEncodeParam;
+                        EncoderParameter CompressionEncodeParam;
+                        EncoderParameters EncoderParams = new EncoderParameters(2);
+
+                        saveEncoder = System.Drawing.Imaging.Encoder.SaveFlag;
+                        compressionEncoder = System.Drawing.Imaging.Encoder.Compression;
+
+                        SaveEncodeParam = new EncoderParameter(saveEncoder, (long)EncoderValue.MultiFrame);
+                        CompressionEncodeParam = new EncoderParameter(compressionEncoder, (long)EncoderValue.CompressionCCITT4);
+                        EncoderParams.Param[0] = CompressionEncodeParam;
+                        EncoderParams.Param[1] = SaveEncodeParam;
+
+                        File.Delete(location);
+                        bmp[0].Save(location, codecInfo, EncoderParams);
+
+                        for (int i = 1; i < bmp.Length; i++)
+                        {
+                            if (bmp[i] == null) break;
+
+                            SaveEncodeParam = new EncoderParameter(saveEncoder, (long)EncoderValue.FrameDimensionPage);
+                            CompressionEncodeParam = new EncoderParameter(compressionEncoder, (long)EncoderValue.CompressionCCITT4);
+                            EncoderParams.Param[0] = CompressionEncodeParam;
+                            EncoderParams.Param[1] = SaveEncodeParam;
+                            bmp[0].SaveAdd(bmp[i], EncoderParams);
+                        }
+
+                        SaveEncodeParam = new EncoderParameter(saveEncoder, (long)EncoderValue.Flush);
+                        EncoderParams.Param[0] = SaveEncodeParam;
+                        bmp[0].SaveAdd(EncoderParams);
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(),"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private ImageCodecInfo get_codec_forstring(string type)
+        {
+            ImageCodecInfo[] info = ImageCodecInfo.GetImageEncoders();
+
+            for (int i = 0; i < info.Length; i++)
+            {
+                string EnumName = type.ToString();
+                if (info[i].FormatDescription.Equals(EnumName))
+                {
+                    return info[i];
+                }
+            }
+            return null;
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            if (!verificar_path()) return;
+            PictureBox pctb1 = new PictureBox();
+            pctb1.Image=Image.FromFile( txtFrontal.Text);
+            PictureBox pctb2 = new PictureBox();
+            pctb2.Image = Image.FromFile(txtDorso.Text);
+            Image[] arrayImages = new Image[2];
+            arrayImages[0] = pctb1.Image;
+            arrayImages[1] = pctb2.Image;
+            string path = ruta_tif + "\\image.tif";
+            DoExistingFileSave(arrayImages,path);
+        }
+
+        private bool verificar_path()
+        {
+            if (txtFrontal.Text.Trim().Equals(""))
+            {
+                errorProvider1.SetError(txtFrontal, "El campo no puede ser blanco");
+                return false;
+            }
+            if (txtDorso.Text.Trim().Equals(""))
+            {
+                errorProvider1.SetError(txtDorso, "El campo no puede ser blanco");
+                return false;
+            }
+            return true;
+        }
+    }
+}
